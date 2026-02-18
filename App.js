15d986091229d93cb7f6e4e0a5f5688ea3444338
johngawin815 <johngawin815@gmail.com>
2026-02-18 12:35:38 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HubShell from './components/layout/HubShell';
+import './App.css'; // Keep your global styles if you have them
+
+function App() {
+  return (
+    <div className="App">
+      <HubShell />
+    </div>
+  );
+}
+
+export default App;
